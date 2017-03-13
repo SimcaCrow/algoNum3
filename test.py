ee763288cacb8de812e48cd292158e083f20d6a3
El-Habr Camille <Simca@localhost.localdomain>
2017-03-13 11:45:23 +0100

    un fichier pour vos tests

---
+++ test.py
@@ -0,0 +1,23 @@
+# ---------------------------------#
+"""
+File : test.py
+Author : Ruello V., Tirel A.
+Description : 3ième TD algorithme numerique
+"""
+# ---------------------------------#
+
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+# ---------------------------------#
+
+
+
+
+# ---------------------------------#
+if __name__ == '__main__':
+	print("Test")
+	
+
+# ---------------------------------#
