305394b1e9918919621c8b49968ac4cb6ffcf07f
Camille El-Habr <cel-ha910e@enseirb-matmeca.fr>
2017-03-24 12:00:34 +0100

    code plus propre

--- householder.py
+++ householder.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
+# ---------------------------------#
+"""
+File : test.py
+Author : Ruello V., Tirel A.
+Description : 3ième TD algorithme numerique
+"""
+# ---------------------------------#
+
 import matplotlib as mp
 import numpy as np
-#     3 4 0
-# 3
-# 4
-# 0
-#         3
-#         4
-#         0
-# 3 4 0
+
+# ---------------------------------#
 
 def householder_explicite(x, y):
     u = (x - y) * 1.0/norme_vecteur(x - y)
@@ -21,9 +23,13 @@ def householder_explicite(x, y):
     H = np.eye(n) - 2 * M
     return H
 
+# ---------------------------------#
+
 def norme_vecteur(u):
     sum_tmp = 0
     n = len(u)
     for i in xrange(n):
         sum_tmp += u[i]**2
     return np.sqrt(sum_tmp)
+
+# ---------------------------------#
