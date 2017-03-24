5131bbebb9e35ca9077ef17b23757c503b2d9d2d
Hystos <viince120.rlo@gmail.com>
2017-03-24 11:31:51 +0100

    Amélioration de l'architecture

--- householder.py
+++ householder.py
@@ -27,15 +27,3 @@ def norme_vecteur(u):
     for i in xrange(n):
         sum_tmp += u[i]**2
     return np.sqrt(sum_tmp)
-
-def test_householder():
-    x = np.array([3., 4., 0.])
-    y = np.array([0.,0.,5.])
-    H = householder_explicite(x, y)
-    H_exp = np.array([ [0.64, -0.48, 0.6], [-0.48, 0.36, 0.8], [0.6, 0.8, 0]])
-    print "Résultat attendu : ", H_exp
-    print "Résultat obtenu : ", H
-    print "Résultat attendu : ",  np.dot(H_exp,x)
-    print "Résultat obtenu : ",  np.dot(H,x)
-
-test_householder()
--- test.py
+++ test.py
@@ -1,10 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
+# coding: utf-8
 # ---------------------------------#
 """
 File : test.py
 Author : Ruello V., Tirel A.
 Description : 3ième TD algorithme numerique
 """
+
+from householder import *
 # ---------------------------------#
 
 import numpy as np
@@ -13,12 +16,20 @@ import matplotlib.pyplot as plt
 
 # ---------------------------------#
 
-
+def test_householder():
+    x = np.array([3., 4., 0.])
+    y = np.array([0.,0.,5.])
+    H = householder_explicite(x, y)
+    H_exp = np.array([ [0.64, -0.48, 0.6], [-0.48, 0.36, 0.8], [0.6, 0.8, 0]])
+    print "Résultat attendu : ", H_exp
+    print "Résultat obtenu : ", H
+    print "Résultat attendu : ",  np.dot(H_exp,x)
+    print "Résultat obtenu : ",  np.dot(H,x)
 
 
 # ---------------------------------#
 if __name__ == '__main__':
-	print("Test")
-	
+	test_householder()
+
 
 # ---------------------------------#
