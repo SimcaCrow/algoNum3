3a555984ad69c66c329edd730a554ee1942b3dd5
Vincent Ruello <vincent.ruello@enseirb-matmeca.fr>
2017-03-28 09:21:39 +0200

    Correction de l'affichage (overflow des valeurs) et mise en place de graphiques sur les tailles

--- image.py
+++ image.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
+# -*- coding: utf-8 -*-
 # ---------------------------------#
 """
 File : image.py
@@ -13,6 +14,7 @@ from qr_transform import *
 from householder import *
 import matplotlib.pyplot as plt
 import matplotlib.image as mpi
+import matplotlib
 
 
 # ---------------------------------#
@@ -21,11 +23,14 @@ def img_plot(matrix):
     plt.imshow(matrix)
     plt.show()
 
+def load_picture(path):
+    return  mpi.imread(path)
+
+
 """
 Renvoie les matrices (r, g, b) de l'image
 """
-def get_image_matrices(path):
-    rgb = mpi.imread(path)
+def get_rgb_matrices(rgb):
     size = rgb.shape
     r = np.zeros((size[0],size[1]))
     g = np.zeros((size[0],size[1]))
@@ -38,15 +43,46 @@ def get_image_matrices(path):
     return (r, g, b)
 
 def matrix_svd(matrix, k):
-    (U, s, V) = np.linalg.svd(matrix, full_matrices=False)
-
-    print U.shape, s.shape, V.shape
+    (U, s, V) = np.linalg.svd(matrix, full_matrices=True)
+    #print U.shape, s.shape, V.shape
     reconstimg = np.matrix(U[:, :k]) * np.diag(s[:k]) * np.matrix(V[:k, :])
+    # print '-----'
+    # ucalc = 1
+    # scalc = 1
+    # vcalc = 1
+    # for x in U[:, :k].shape:
+    #     ucalc *= x
+    # for x in s[:k].shape:
+    #     scalc *= x
+    # for x in V[:k, :].shape:
+    #     vcalc *= x
+    # print "Taille = ", ucalc + scalc + vcalc
+    #
+    # print np.matrix(U[:, :k]).shape
+    # print np.diag(s[:k]).shape
+    # print np.matrix(V[:k, :]).shape
+    # print reconstimg.shape
+    for i in xrange(reconstimg.shape[0]):
+        for j in xrange(reconstimg.shape[1]):
+            if reconstimg[i,j] > 1:
+                reconstimg[i,j] = 1
+            elif reconstimg[i,j] < 0:
+                reconstimg[i,j] = 0
     return reconstimg
 
-def svd_compress(k):
-    (r, g, b) = get_image_matrices('rocket.png')
-    print r
+def compute_compressed_size(img, k):
+    compressedSize = 0
+    compressedSize += img.shape[0] * k
+    compressedSize += k
+    compressedSize += k * img.shape[1]
+    return compressedSize * 3
+
+def compute_source_image_size(img):
+    return img.shape[0] * img.shape[1] * img.shape[2]
+
+
+def svd_compress(src, k):
+    (r, g, b) = get_rgb_matrices(src)
     rc = matrix_svd(r, k)
     gc = matrix_svd(g, k)
     bc = matrix_svd(b, k)
@@ -57,8 +93,55 @@ def svd_compress(k):
             img[i,j,1] = gc[i,j]
             img[i,j,2] = bc[i,j]
     img_plot(img)
+    return img
+
+
+def graph_size(img, start = 0, end = 200, step = 1):
+    X = range(start, end, step)
+    Y = list()
+    Z = [compute_source_image_size(img)] *  ((end-start) / step)
+    for k in range(start, end, step):
+        Y.append(compute_compressed_size(img, k))
+    plt.title(u"Graphique représentant la taille de l'image compressée en fonction du rang")
+    plt.ylabel(u"Taille de l'image compressée au rang k")
+    plt.xlabel("k")
+    plt.plot(X, Y)
+    plt.plot(X, Z)
+    plt.show()
+
+def graph_size_efficienty(img, start = 0, end = 500, step = 1):
+    X = range(start, end, step)
+    Y = list()
+    srcSize = compute_source_image_size(img)
+    for k in range(start, end, step):
+        Y.append(compute_compressed_size(img, k) * 1.0 / srcSize)
+    plt.title(u"Graphique représentant le rapport entre la taille de l'image compressée et la taille de l'image d'origine en fonction du rang")
+    plt.ylabel(u"Rapport entre la taille de l'image compressée et la taille de l'image d'origine au rang k")
+    plt.xlabel("k")
+    plt.plot(X, Y)
+    plt.show()
+
+def graph_error_algebric(img, start = 0, end = 500, step = 1):
+    X = range(start, end, step)
+    Y = list()
+
+    for k in range(start, end, step):
+        Y.append(compute_compressed_size(img, k) * 1.0 / srcSize)
+    plt.title(u"Graphique représentant le rapport entre la taille de l'image compressée et la taille de l'image d'origine en fonction du rang")
+    plt.ylabel(u"Rapport entre la taille de l'image compressée et la taille de l'image d'origine au rang k")
+    plt.xlabel("k")
+    plt.plot(X, Y)
+    plt.show()
+
 # ---------------------------------#
 if __name__ == '__main__':
-    svd_compress(200)
+    k = 5
+    imageName = 'rocket.png'
+
+    imgSrc = load_picture(imageName)
+    svd_compress(imgSrc, k)
+    graph_size(imgSrc)
+    graph_size_efficienty(imgSrc)
+
 
 # ---------------------------------#
