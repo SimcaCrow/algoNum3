6ff46131145eb5274bfc935b5e5168097feeb980
Vincent Ruello <vincent.ruello@enseirb-matmeca.fr>
2017-04-01 16:40:22 +0200

    Fin de image.py (dernières partie), échec de la partie 2...

--- householder.py
+++ householder.py
@@ -14,15 +14,24 @@ import numpy as np
 # ---------------------------------#
 
 def householder_explicite(x, y):
-    u = (x - y) * 1.0/norme_vecteur(x - y)
+    u = x - y
+    return householder(u)
+
+def householder(u):
     n = len(u)
     M = np.zeros((n,n))
     for i in xrange(n):
         for j in xrange(n):
             M[i,j] = u[i] * u[j]
-    H = np.eye(n) - 2 * M
+    H = np.eye(n) - 2 * M * (1.0/norme_vecteur(u)**2)
     return H
 
+def make_householder(a):
+    v = a / (a[0] + np.copysign(np.linalg.norm(a), a[0]))
+    v[0] = 1
+    H = np.eye(a.shape[0])
+    H -= (2 / np.dot(v, v)) * np.dot(v[:, None], v[None, :])
+    return H
 # ---------------------------------#
 
 def norme_vecteur(u):
